5eb098c6588a20163a2fa0f1c5f797b792ad8a9b
SilverAndrels <andresilva64@outlook.com>
2022-08-07 14:49:15 -0300

    Change names

--- geradordesenhas.py
+++ geradordesenhas.py
@@ -1,5 +1,5 @@
 import random, string
-tamanho = 16
+size = 16
 chars = string.ascii_letters + string.digits + 'ç!@#$%&*()-+=,.;:/?'
 rdm = random.SystemRandom()
-print("Password: " + ''.join(rdm.choice(chars) for i in range(tamanho)))
+print("Password: " + ''.join(rdm.choice(chars) for i in range(size)))
